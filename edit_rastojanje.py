cdbdcaea560433d815d34ec59e8a1f4f9af2e337
vojann <48259582+vojann@users.noreply.github.com>
2020-03-16 15:06:17 +0100

    Update edit_rastojanje.py

--- edit_rastojanje.py
+++ edit_rastojanje.py
@@ -2,20 +2,23 @@
 # npr abbc i babb, program ce vratiti 2 jer mozemo dopisati *b*abbc i potom ukloniti poslednje c
 import numpy as np
 def rastojanje(A,B):
-    n=len(A)
-    m=len(B)
-    C = np.zeros((n + 1, m + 1))
+    n = len(A)
+    m = len(B)
+    matrix = np.zeros((n + 1, m + 1))
     for i in range(n + 1):
-        C[i][0] = i
+        matrix[i][0] = i
     for j in range(m + 1):
-        C[0][j] = j
+        matrix[0][j] = j
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            x = C[i - 1][j] + 1
-            y = C[i][j - 1] + 1
+            x = matrix[i - 1][j] + 1
+            y = matrix[i][j - 1] + 1
             if A[i - 1] == B[j - 1]:
-                z = C[i - 1][j - 1]
+                z = matrix[i - 1][j - 1]
             else:
-                z = C[i - 1][j - 1] + 1
-            C[i][j] = min(x, y, z)
-    return int(C[n][m])
+                z = matrix[i - 1][j - 1] + 1
+            matrix[i][j] = min(x, y, z)
+    return int(matrix[n][m])
+
+
+
