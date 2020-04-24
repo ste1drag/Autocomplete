893e120cd310c1e289df350d75aa696f7edf9d3f
vojann <48259582+vojann@users.noreply.github.com>
2020-04-24 23:14:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from tkinter import *
 import sys
+import webbrowser
 
+
+import numpy as np
+
+def rastojanje(A,B):
+    n = len(A)
+    m = len(B)
+    matrix = np.zeros((n + 1, m + 1))
+    for i in range(n + 1):
+        matrix[i][0] = i
+    for j in range(m + 1):
+        matrix[0][j] = j
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            x = matrix[i - 1][j] + 1
+            y = matrix[i][j - 1] + 1
+            if A[i - 1] == B[j - 1]:
+                z = matrix[i - 1][j - 1]
+            else:
+                z = matrix[i - 1][j - 1] + 1
+            matrix[i][j] = min(x, y, z)
+    return int(matrix[n][m])
 
 def lexicographic_sort(lista):
     lista.sort(key=lambda x:x[0])
@@ -11,7 +33,7 @@ def weight_sort(lista):
     return lista
 
 def sort_first_r(lista,r):
-    lista.sort(key=lambda x:x[0][0:len(r)])
+    lista.sort(key=lambda x:x[0][0:r])
 
 def binarna_pretraga(uzorak,lista):
     l=0
@@ -29,7 +51,9 @@ def binarna_pretraga(uzorak,lista):
 
 
 
-f=open(sys.argv[1])
+
+f=open(sys.argv[1],encoding="utf-8")
+
 
 lista=[]
 
@@ -44,19 +68,14 @@ for linija in f:
     torka=(text[1],text[0])
     lista.append(torka)
 
-print(lista)
+
 
 
 #s=input()
-s=input()
-pom_lista=lista
-bin_lista=[]
-lexicographic_sort(pom_lista)
-x=binarna_pretraga(s,pom_lista)
-while x!=-1:
-    bin_lista.append(x)
-    pom_lista.remove(x)
-    x=binarna_pretraga(s,pom_lista)
+
+
+n1=sys.argv[2]
+br=int(n1)
 
 
 
@@ -72,29 +91,111 @@ Label(root).grid(row=0,column=2)
 Label(root).grid(row=0,column=3)
 Label(root).grid(row=0,column=4)
 Label(root).grid(row=0,column=5)
+Label(root).grid(row=0,column=6)
+Label(root).grid(row=0,column=7)
+Label(root).grid(row=0,column=8)
 Label(root).grid(row=1,column=0)
 Label(root).grid(row=1,column=1)
 Label(root).grid(row=1,column=2)
 Label(root).grid(row=1,column=3)
 l1=Label(root,text="Search query:",bg="white",fg="black").grid(row=1,column=4)
-Label(root).grid(row=1,column=5)
-te=Entry(root,width=70)
-te.grid(row=1,column=6)
+
+sv=StringVar()
+
+
+def callback(sv,lista,br):
+    a=sv.get().strip()
+    pom_lista = lista[:]
+    bin_lista = []
+    lexicographic_sort(pom_lista)
+    x = binarna_pretraga(a, pom_lista)
+    while x != -1:
+        bin_lista.append(x)
+        pom_lista.remove(x)
+        x = binarna_pretraga(a, pom_lista)
+
+    #if len(bin_lista) == 0:
+     #   for el in lista:
+      #      if rastojanje(el[0][0:len(a)], a) < 2:
+       #         bin_lista.append(el)
+
+    i = 0
+    l = Listbox(root, width=40)
+    l.grid(row=2, column=5)
+    weight_sort(bin_lista)
+    if len(bin_lista) == 0 or len(a) == 0:
+        l.delete(0, END)
+
+
+    else:
+        for list in bin_lista:
+            if i == br:
+                break
+            if var1.get()==1:
+                l.insert(END,list[0]+" "+str(list[1]).rjust(50))
+            else:
+                l.insert(END,list[0])
+            i += 1
+
+    def CurSelet(evt):
+        try:
+            value = str((l.get(l.curselection())))
+            pom=""
+            for c in value:
+                if not c.isdecimal():
+                    pom+=c
+            sv.set(pom)
+        except(TclError):
+            pass
+
+
+
+
+    l.bind('<<ListboxSelect>>', CurSelet)
+
+
+
+te = Entry(root, textvariable=sv, width=40)
+
+def Klik():
+    webbrowser.open("www.google.com/search?q=" + te.get())
+
+Button(root, text='Search Google', command=Klik).grid(row=1, column=8)
+
+
+
+
+
+
+lexicographic_sort(lista)
+
+
+sv.trace("w", lambda name, index, mode, sv=sv: callback(sv,lista,br))
+
+te.grid(row=1,column=5)
+Label(root).grid(row=1,column=6)
 Label(root).grid(row=1,column=7)
-Label(root).grid(row=1,column=8)
 
 
-def prikaziSugestije():
-    exit()
 
 
-Button(root,text='Search Google', command=prikaziSugestije).grid(row=1,column=9)
+
+
 Label(root).grid(row=2,column=0)
 Label(root).grid(row=2,column=1)
 Label(root).grid(row=2,column=2)
-Label(root).grid(row=2,column=3)
 var1=IntVar()
-Checkbutton(root, text="Show weights", variable=var1).grid(row=2, column=4)
+Checkbutton(root, text="Show weights", variable=var1).grid(row=6, column=3)
+Label(root).grid(row=7,column=3)
+Label(root).grid(row=8,column=3)
+Label(root).grid(row=9,column=3)
+Label(root).grid(row=10,column=3)
+Label(root).grid(row=11,column=3)
+
+
+
+
+
 root.mainloop()
 
 
