ed7f41d2139b28d90e71f2e82395eef6c97fc072
vojann <48259582+vojann@users.noreply.github.com>
2020-05-09 19:39:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from tkinter import *
 import sys
+from tkinter import *
+from tkinter import ttk
 import webbrowser
+from tkinter.messagebox import askyesno
+import tkinter.font as font
+
 import numpy as np
+
+
+
+def lexicographic_sort(lista):
+    lista.sort(key=lambda x:x[0])
+    return lista
+
+def weight_sort(lista):
+    lista.sort(key=lambda x:x[1],reverse=True)
+    return lista
+
+def sort_first_r(lista,r):
+    lista.sort(key=lambda x:x[0][0:r])
+
+def binarna_pretraga(uzorak,lista):
+    l=0
+    r=len(lista)-1
+    while l<=r:
+        m=(r-l)/2+l
+        m=int(m)
+        if lista[m][0][0:len(uzorak)]==uzorak:
+            return lista[m]
+        elif lista[m][0][0:len(uzorak)]<uzorak:
+            l=m+1
+        elif lista[m][0][0:len(uzorak)]>uzorak:
+            r=m-1
+    return -1
+
 def rastojanje(A,B):
     n = len(A)
     m = len(B)
@@ -20,29 +52,13 @@ def rastojanje(A,B):
                 z = matrix[i - 1][j - 1] + 1
             matrix[i][j] = min(x, y, z)
     return int(matrix[n][m])
-def lexicographic_sort(lista):
-    lista.sort(key=lambda x:x[0])
-    return lista
-def weight_sort(lista):
-    lista.sort(key=lambda x:x[1],reverse=True)
-    return lista
-def sort_first_r(lista,r):
-    lista.sort(key=lambda x:x[0][0:r])
-def binarna_pretraga(uzorak,lista):
-    l=0
-    r=len(lista)-1
-    while l<=r:
-        m=(r-l)/2+l
-        m=int(m)
-        if lista[m][0][0:len(uzorak)]==uzorak:
-            return lista[m]
-        elif lista[m][0][0:len(uzorak)]<uzorak:
-            l=m+1
-        elif lista[m][0][0:len(uzorak)]>uzorak:
-            r=m-1
-    return -1
-f=open(sys.argv[1],encoding="utf-8")
+
+try:
+    f=open(sys.argv[1],encoding="utf-8")
+except(IndexError):
+    exit("Navedite datoteku")
 lista=[]
+
 for linija in f:
     text=linija.split()
     if len(text)<2:
@@ -53,14 +69,45 @@ for linija in f:
             text[1]+=" "+text[i]
     torka=(text[1],text[0])
     lista.append(torka)
-n1=sys.argv[2]
-br=int(n1)
+try:
+    n1=sys.argv[2]
+    br=int(n1)
+except(IndexError):
+    exit("Navedite broj")
+
+
+
+
+
 root=Tk()
+
 root.title("Autocomplete me")
-root.geometry("500x250")
+root.geometry("940x370")
+
+style=ttk.Style()
+style.configure("TButton", padding=6, relief="flat",font=('Helvetica', 12))
+
+photo=PhotoImage(file="novaslika.png")
+label=Label(root,image=photo)
+label.place(x=260,y=15)
+
+style.configure('TEntry', padding=6, relief="flat",font=('Courier', 15))
+
+photo=PhotoImage(file="novaslika.png")
+label=Label(root,image=photo)
+label.place(x=350,y=15)
+mojFont=font.Font(family='Courier',size=13)
+
 l1=Label(root,text="Search query:",bg="white",fg="black")
-l1.place(x=35,y=1)
+l1['font']=mojFont
+l1.place(x=35,y=120)
 sv=StringVar()
+def Klik():
+    webbrowser.open("www.google.com/search?q="+te.get())
+
+b=ttk.Button( text='Search Google',command=Klik)
+b.place(x=783,y=110)
+
 def callback(sv,lista,br):
     a=sv.get().strip()
     pom_lista = lista[:]
@@ -72,8 +119,14 @@ def callback(sv,lista,br):
         pom_lista.remove(x)
         x = binarna_pretraga(a, pom_lista)
     i = 0
-    l = Listbox(root, width=40)
-    l.place(x=120,y=20)
+
+    if not bin_lista:
+        for el in pom_lista:
+            if rastojanje(te.get(),el[0])<2 and len(te.get())==len(el[0]):
+                 bin_lista.append(el)
+
+    l = Listbox(root, width=73,font = ('courier', 10))
+    l.place(x=190,y=153)
     weight_sort(bin_lista)
     if len(bin_lista) == 0 or len(a) == 0:
             l.delete(0, END)
@@ -82,31 +135,32 @@ def callback(sv,lista,br):
             if i == br:
                 break
             if var1.get()==1:
-
-                l.insert(END,"{:<10}   {:>55}".format(list[0],str(list[1])))
+                tmp1=list[0].strip()
+                tmp2=str(list[1]).strip()
+                l.insert(END,f'{" "+tmp1:<{55}} {tmp2:>{15}}')
             else:
                 l.insert(END,list[0])
             i += 1
+
     def CurSelet(evt):
         try:
             value = str((l.get(l.curselection())))
-            pom=""
+            pom = ""
             for c in value:
                 if not c.isdecimal():
-                    pom+=c
+                    pom += c
             sv.set(pom)
         except(TclError):
             pass
+
     l.bind('<<ListboxSelect>>', CurSelet)
-te = Entry(root, textvariable=sv, width=40)
-te.place(x=120,y=1)
-def Klik():
-    webbrowser.open("www.google.com/search?q=" + te.get())
-b=Button(root, text='Search Google',height=1, command=Klik)
-b.place(x=370,y=1)
+
 lexicographic_sort(lista)
 sv.trace("w", lambda name, index, mode, sv=sv: callback(sv,lista,br))
+te = ttk.Entry(textvariable=sv, width=71,font = ('courier', 10))
+te.place(x=190,y=115)
 var1=IntVar()
 c=Checkbutton(root, text="Show weights", variable=var1)
-c.place(x=5,y=25)
+c['font']=mojFont
+c.place(x=20,y=160)
 root.mainloop()
